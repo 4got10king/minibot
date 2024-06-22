7ef9d235a1e38e422562c876abd966ef8fb97de3
4got10king <Aldavletshina77654@yandex.ru>
2024-06-22 20:21:22 +0300

    Add new func

--- ltchak/bot.py
+++ ltchak/bot.py
@@ -23,26 +23,35 @@ async def cmd_start(message: types.Message):
     )
 
 
+async def get_data_from_multiple_sources(api_key, inn):
+    data = {}
+
+    endpoints = ["company", "legal-cases", "enforcements", "finances"]
+
+    for endpoint in endpoints:
+        url = f"https://api.checko.ru/v2/{endpoint}?key={api_key}&inn={inn}"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            endpoint_data = response.json()
+            data[endpoint] = endpoint_data
+
+    with open("data.json", "a", encoding="utf-8") as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
+
+    return data
+
+
 @dp.message(lambda message: message.text.isdigit())
-async def get_company_info(message: types.Message):
+async def get_all_data(message: types.Message):
     api_key = "amp5SkbhXJOrm8g8"
     inn = message.text
 
-    url = f"https://api.checko.ru/v2/company?key={api_key}&inn={inn}"
-    response = requests.get(url)
+    all_data = await get_data_from_multiple_sources(api_key, inn)
 
-    if response.status_code == 200:
-        data = response.json()
-
-        with open("data.json", "w", encoding="utf-8") as file:
-            json.dump(data, file, indent=4, ensure_ascii=False)
-
-        chat_id = message.chat.id
-        document = FSInputFile("data.json")
-        await bot.send_document(document=document, chat_id=chat_id)
-
-    else:
-        await message.answer("Ошибка при выполнении запроса")
+    chat_id = message.chat.id
+    document = FSInputFile("data.json")
+    await bot.send_document(document=document, chat_id=chat_id)
 
 
 async def main():
